c09df88318e152f3454d51e50136888987f049a9
EntFladda <adadgamer01@gmail.com>
2024-12-11 03:05:30 +0700

    Fix custom carousel looping behavior; adjust page navigation to avoid dummy first page and ensure seamless transitions

--- lib/widgets/custom_carousel.dart
+++ lib/widgets/custom_carousel.dart
@@ -42,7 +42,7 @@ class _CustomCarouselState extends State<CustomCarousel> {
       if (_pageController.hasClients && banners != null) {
         int nextPage = _pageController.page!.toInt() + 1;
         if (nextPage >= banners!.length + 1) {
-          nextPage = 0;
+          nextPage = 1; // Set to 1 to avoid the dummy first page
         }
         _pageController.animateToPage(
           nextPage,
@@ -58,13 +58,13 @@ class _CustomCarouselState extends State<CustomCarousel> {
       currentPage = index;
     });
 
-    if (index == bannerCount) {
+    if (index == bannerCount + 1) {
       Future.delayed(const Duration(milliseconds: 300), () {
-        _pageController.jumpToPage(0);
+        _pageController.jumpToPage(1);
       });
-    } else if (index == -1) {
+    } else if (index == 0) {
       Future.delayed(const Duration(milliseconds: 300), () {
-        _pageController.jumpToPage(bannerCount - 1);
+        _pageController.jumpToPage(bannerCount);
       });
     }
   }
@@ -90,20 +90,27 @@ class _CustomCarouselState extends State<CustomCarousel> {
                   controller: _pageController,
                   onPageChanged: (index) =>
                       _onPageChanged(index, banners.length),
-                  itemCount: banners.length + 1,
+                  itemCount: banners.length + 2, // Add 2 for looping
                   itemBuilder: (context, index) {
-                    if (index == banners.length) {
+                    if (index == 0) {
+                      return CustomBanner(
+                        imagePath: banners[banners.length - 1]['imagePath']!,
+                        title: banners[banners.length - 1]['title']!,
+                        subtitle: banners[banners.length - 1]['subtitle']!,
+                      );
+                    } else if (index == banners.length + 1) {
                       return CustomBanner(
                         imagePath: banners[0]['imagePath']!,
                         title: banners[0]['title']!,
                         subtitle: banners[0]['subtitle']!,
                       );
+                    } else {
+                      return CustomBanner(
+                        imagePath: banners[index - 1]['imagePath']!,
+                        title: banners[index - 1]['title']!,
+                        subtitle: banners[index - 1]['subtitle']!,
+                      );
                     }
-                    return CustomBanner(
-                      imagePath: banners[index]['imagePath']!,
-                      title: banners[index]['title']!,
-                      subtitle: banners[index]['subtitle']!,
-                    );
                   },
                 ),
               ),
