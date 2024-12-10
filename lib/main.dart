a5718cbb77f3a1beb510bd35165f375ba633f15c
EntFladda <adadgamer01@gmail.com>
2024-12-11 05:04:51 +0700

    Implement login screen and user profile management; add error handling and user data fetching

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:filmify/screens/login.dart';
+import 'package:filmify/screens/profile.dart';
 import 'package:filmify/screens/splash.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -18,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      home: Login(),
       title: "Filmify",
     );
   }
--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,12 +1,76 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:filmify/screens/change_password.dart';
 import 'package:filmify/screens/login.dart';
 import 'package:filmify/utils/colors.dart';
 import 'package:filmify/widgets/custom_button.dart';
 import 'package:filmify/widgets/custom_menu_profile.dart';
 import 'package:flutter/material.dart';
+import 'package:filmify/services/auth_service.dart';
 
-class Profile extends StatelessWidget {
+class Profile extends StatefulWidget {
   const Profile({super.key});
+
+  @override
+  _ProfileState createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final AuthService _authService = AuthService();
+
+  String username = '';
+  String email = '';
+  String age = '';
+
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _ageController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchUserData();
+  }
+
+  Future<void> _fetchUserData() async {
+    User? user = _auth.currentUser;
+    if (user != null) {
+      DocumentSnapshot userDoc =
+          await _firestore.collection('users').doc(user.uid).get();
+      setState(() {
+        username = userDoc['username'];
+        email = userDoc['email'];
+        age = userDoc['age'].toString();
+        _usernameController.text = username;
+        _ageController.text = age;
+      });
+    }
+  }
+
+  Future<void> _updateUserProfile() async {
+    User? user = _auth.currentUser;
+    if (user != null) {
+      Map<String, dynamic> updatedData = {};
+      if (_usernameController.text.isNotEmpty) {
+        updatedData['username'] = _usernameController.text;
+      }
+      if (_ageController.text.isNotEmpty) {
+        updatedData['age'] = _ageController.text;
+      }
+
+      if (updatedData.isNotEmpty) {
+        await _authService.updateUserProfile(user.uid, updatedData);
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Profile updated successfully')),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('No changes to update')),
+        );
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,17 +102,24 @@ class Profile extends StatelessWidget {
                 "Username",
                 style: TextStyle(
                   fontSize: 16,
-                  color: customTeksColorDark, // Gunakan parameter warna
+                  color: customTeksColorDark,
                 ),
               ),
             ),
             const SizedBox(height: 5),
             TextField(
+              controller: _usernameController,
+              style: const TextStyle(
+                color: Colors.black, // Black color when typing
+              ),
               decoration: InputDecoration(
                 filled: true,
                 fillColor: Colors.white,
-                hintText: "Username",
-                hintStyle: const TextStyle(color: Colors.black45),
+                hintText: username,
+                hintStyle: const TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(10),
                 ),
@@ -63,17 +134,24 @@ class Profile extends StatelessWidget {
                 "Email",
                 style: TextStyle(
                   fontSize: 16,
-                  color: customTeksColorDark, // Gunakan parameter warna
+                  color: customTeksColorDark,
                 ),
               ),
             ),
             const SizedBox(height: 5),
             TextField(
+              enabled: false,
+              style: const TextStyle(
+                color: Colors.black, // Black color when typing
+              ),
               decoration: InputDecoration(
                 filled: true,
                 fillColor: Colors.white,
-                hintText: "example@example.com",
-                hintStyle: const TextStyle(color: Colors.black45),
+                hintText: email,
+                hintStyle: const TextStyle(
+                  color: Colors.grey, // Lighter color to indicate non-editable
+                  fontWeight: FontWeight.bold,
+                ),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(10),
                 ),
@@ -88,17 +166,24 @@ class Profile extends StatelessWidget {
                 "Age",
                 style: TextStyle(
                   fontSize: 16,
-                  color: customTeksColorDark, // Gunakan parameter warna
+                  color: customTeksColorDark,
                 ),
               ),
             ),
             const SizedBox(height: 5),
             TextField(
+              controller: _ageController,
+              style: const TextStyle(
+                color: Colors.black, // Black color when typing
+              ),
               decoration: InputDecoration(
                 filled: true,
                 fillColor: Colors.white,
-                hintText: "Age",
-                hintStyle: const TextStyle(color: Colors.black45),
+                hintText: age,
+                hintStyle: const TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(10),
                 ),
@@ -108,7 +193,7 @@ class Profile extends StatelessWidget {
             ),
             const SizedBox(height: 30),
             CustomButton(
-              onPressed: () {},
+              onPressed: _updateUserProfile,
               text: 'Update',
               backgroundColor: customButtonColorDark,
               textColor: customTeksColorLight,
