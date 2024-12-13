89cbb7bc3db7b1d2368dbf36c9c9545d0b0f848a
EntFladda <adadgamer01@gmail.com>
2024-12-13 21:49:04 +0700

    add splash screen import to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:filmify/screens/home.dart';
+import 'package:filmify/screens/splash.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
